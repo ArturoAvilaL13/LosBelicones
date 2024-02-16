05cf6f27a324843f2e408aca53ab3ee4907e5b0b
Arturo Avila <150939606+ArturoAvilaL13@users.noreply.github.com>
2024-02-16 11:15:43 -0600

    menu

--- modules/urlBase.js
+++ modules/urlBase.js
@@ -1,5 +1,5 @@
 //~URL base para la api
-// const baseURL = "http://localhost:8080/api/v1/";
-const baseURL = "https://api-wr11.onrender.com/api/v1/";
+const baseURL = "http://localhost:8080/api/v1/";
+// const baseURL = "https://api-wr11.onrender.com/api/v1/";
 
 export { baseURL };
